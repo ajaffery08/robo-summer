26cc7b534f2dddf7bec110ee03276b4e71832481
Ahmed Jaffery <ajaffery08@gmail.com>
2021-06-10 20:20:36 -0400

    Initial App Setup
    Initial app setup including:
    -requirements.txt file
    -.env file
    -stock time series API for MSFT
    -using my own API key

---
+++ robo_advisor.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+
+symbol = "MSFT" #ask for user input
+ALPHAVANTAGE_API_KEY = "5YER38UNGIAFMX3B"
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
