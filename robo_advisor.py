dce023916a2bc6113a15dc142913507e0b19ba12
Ahmed Jaffery <ajaffery08@gmail.com>
2021-06-10 20:28:15 -0400

    Update to robo_advisor.py
    Updated the .py file to move the API key to the .env file and call that key as a separate variable. This is so the key is not public in Github.

--- robo_advisor.py
+++ robo_advisor.py
@@ -4,9 +4,8 @@ import requests
 
 load_dotenv()
 
-
 symbol = "MSFT" #ask for user input
-ALPHAVANTAGE_API_KEY = "5YER38UNGIAFMX3B"
+ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
 
 response = requests.get(request_url)
